d581399b1d86cb49f7c35986a58b532326c9dbe5
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-06-29 01:58:23 -0500

    added frontend dependencies

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,9 @@
+import 'bootswatch/dist/lux/bootstrap.min.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
     <App />,
