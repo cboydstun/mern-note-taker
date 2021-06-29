ea4c43d74aa16043e101614b4b2f24bf42012eb4
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-06-29 01:34:37 -0500

    added note model

---
+++ server/models/Note.js
@@ -0,0 +1,25 @@
+const {Schema,model, models} = require('mongoose');
+
+const noteSchema = new Schema({
+    author:{
+        type:String,
+        required:true
+    },
+    title:{
+        type:String,
+        required:true
+    },
+    content:{
+        type:String,
+        required:true
+    },
+    priority:{
+        type:String
+    },
+    date:{
+        type:Date,
+        default:Date.now
+    }   
+},{timestamps:true})
+
+module.exports = model('Note', noteSchema);
